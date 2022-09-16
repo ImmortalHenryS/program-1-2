077329e9e8d2dd1ce20d2d57247ce5ab091804ec
ImmortalHenryS <112955164+ImmortalHenryS@users.noreply.github.com>
2022-09-16 14:34:42 +0000

    indent update (again)

--- hellobeep.py
+++ hellobeep.py
@@ -2,72 +2,73 @@ from stanfordkarel import *
 
 
 class ktools:
- def m(self):
-   """M shorthand for move"""
- move()
+   def m(self):
+     """M shorthand for move"""
+     move()
 
-def tl(self):
-  turn_left
-
-
-def tr(self):
-    """turn around"""
-    self.tl()
-    self.tl()
-    self.tl()
-
-def pick(self):
-    pick_beeper
-
-def put(self):
-    put_beeper
+   def tl(self):
+    turn_left
 
 
-def put2(self):
- self.put()
- self.m()
- self.put()
+   def tr(self):
+        """turn around"""
+        self.tl()
+        self.tl()
+        self.tl()
+
+   def pick(self):
+      pick_beeper
+
+   def put(self):
+     put_beeper
 
 
-def put5(self):
- self.put2()
- self.m()
- self.put2()
- self.m()
- self.put()
-
-def h(self):
- self.tl()
- self.tr()
- self.m()
- self.m()
- self.m()
- self.tr()
- self.put5()
- self.ta()
- self.m()
- self.m()
- self.tl()
- self.m()
- self.put2()
- self.tl()
- self.m()
- self.m()
- self.tl()
- self.m()
- self.m()
- self.m()
- self.m()
+   def put2(self):
+     self.put()
+     self.m()
+     self.put()
 
 
+   def put5(self):
+     self.put2()
+     self.m()
+     self.put2()
+     self.m()
+     self.put()
+
+   def h(self):
+     self.tl()
+     self.tr()
+     self.m()
+     self.m()
+     self.m()
+     self.tr()
+     self.put5()
+     self.ta()
+     self.m()
+     self.m()
+     self.tl()
+     self.m()
+     self.put2()
+     self.tl()
+     self.m()
+     self.m()
+     self.tl()
+     self.m()
+     self.m()
+     self.m()
+     self.m()
+
+    def e(self):
+ 
 
 
 
 
 def main():
    """ Karel code goes here! """
-   kt=ktools() 
-   
+   kt=ktools()
+   kt.h()
    pass
 
 
