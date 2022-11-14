640e9ba536363fae6007e03e5e3ef9698038cee0
ImmortalHenryS <112955164+ImmortalHenryS@users.noreply.github.com>
2022-11-14 15:16:05 +0000

    update

---
+++ slice.py
@@ -0,0 +1,10 @@
+list1 = [1,2,3,4,5]
+list2 = list1[0:3]
+print(list2)
+list3 = list1[3:len(list1)]
+print(list3)
+
+strHello = "Hello, world!"
+substr = steHello[0:5]
+print(substr)
+print=strHello
